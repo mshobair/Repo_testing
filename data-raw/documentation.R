63fb94e63b728ab3e0887dee4561d89a90423d8d
mshobair <mshobair1990@gmail.com>
2019-11-17 03:23:14 -0500

    first commit

---
+++ data-raw/documentation.R
@@ -0,0 +1,32 @@
+#' mtcars20
+#' A data package for mtcars20.
+#' @docType package
+#' @aliases mtcars20-package
+#' @title Package Title
+#' @name mtcars20
+#' @description A description of the data package
+#' @details Use \code{data(package='mtcars20')$results[, 3]} to see a list of available data sets in this data package
+#'     and/or DataPackageR::load_all
+#' _datasets() to load them.
+#' @seealso
+#' \link{cars_over_20}
+NULL
+
+
+
+#' Detailed description of the data
+#' @name cars_over_20
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{data.frame} containing the following fields:
+#' \describe{
+#' \item{speed}{}
+#' \item{dist}{}
+#' }
+#' @source The data comes from________________________.
+#' @seealso
+#' \link{mtcars20}
+NULL
+
+
+
